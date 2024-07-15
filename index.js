dea39cde5964253a8d0b6c4e5072a0ec7d5e49f8
Isaiah <zaymgmt707@gmail.com>
2024-07-14 19:30:52 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+// creates an array of cats
 const cats = ["Milo", "Otis", "Garfield"]
+// Adds the name to the end of the cats array
 function destructivelyAppendCat(name) {
     cats.push(name)
 }
+// Adds the name to the beginning of the cats array
 function destructivelyPrependCat(name) {
     cats.unshift(name)
 }
+// Removes last element from the cats array
 function destructivelyRemoveLastCat() {
     cats.pop()
 }
+// Removes fist element from cats array
 function destructivelyRemoveFirstCat() {
     cats.shift()
 }
+//Returns a new array with the name added to the end
 function appendCat(name) {
     return [...cats, name]
 }
+// Returns a new array with the name added to the beginning
 function prependCat(name) {
     return [name, ...cats]
 }
+// Returns a new array without the last element
 function removeLastCat() {
     return cats.slice(0, -1)
 }
+//Returns a new array without the first element
 function removeFirstCat() {
     return cats.slice(1)
 }
